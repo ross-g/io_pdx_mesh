66d7b307b2a1aeac01d3f2c4d2838fb833e0601a
ross-g <ross.garfoot@gmail.com>
2017-02-05 16:20:50 +0000

    Creating repo
    Adding data and i/o modules.

---
+++ maya_import_export.py
@@ -0,0 +1,262 @@
+"""
+    Paradox asset files, Maya import/export.
+    
+    author : ross-g
+"""
+
+import os, sys
+import maya.cmds as cmds
+import pymel.core as pmc
+import pymel.core.datatypes as pmdt
+import maya.OpenMaya as OpenMaya    # Maya Python API 1.0
+import maya.api.OpenMaya as OpenMaya2    # Maya Python API 2.0
+
+sys.path.append(r"C:\Users\Ross\Documents\GitHub\io_pdx_mesh")
+try: reload(pdx_data)
+except: import pdx_data
+
+
+""" ================================================================================================
+    Functions.
+====================================================================================================
+"""
+
+def create_FileTexture(tex_filepath):
+    """
+        Creates & connects up a new file node and place2dTexture node, uses the supplied filepath.
+    """
+    newFile = pmc.shadingNode('file', asTexture=True)
+    new2dTex = pmc.shadingNode('place2dTexture', asUtility=True)
+
+    pmc.connectAttr(new2dTex.coverage, newFile.coverage)
+    pmc.connectAttr(new2dTex.translateFrame, newFile.translateFrame)
+    pmc.connectAttr(new2dTex.rotateFrame, newFile.rotateFrame)
+    pmc.connectAttr(new2dTex.mirrorU, newFile.mirrorU)
+    pmc.connectAttr(new2dTex.mirrorV, newFile.mirrorV)
+    pmc.connectAttr(new2dTex.stagger, newFile.stagger)
+    pmc.connectAttr(new2dTex.wrapU, newFile.wrapU)
+    pmc.connectAttr(new2dTex.wrapV, newFile.wrapV)
+    pmc.connectAttr(new2dTex.repeatUV, newFile.repeatUV)
+    pmc.connectAttr(new2dTex.offset, newFile.offset)
+    pmc.connectAttr(new2dTex.rotateUV, newFile.rotateUV)
+    pmc.connectAttr(new2dTex.noiseUV, newFile.noiseUV)
+    pmc.connectAttr(new2dTex.vertexUvOne, newFile.vertexUvOne)
+    pmc.connectAttr(new2dTex.vertexUvTwo, newFile.vertexUvTwo)
+    pmc.connectAttr(new2dTex.vertexUvThree, newFile.vertexUvThree)
+    pmc.connectAttr(new2dTex.vertexCameraOne, newFile.vertexCameraOne)
+    pmc.connectAttr(new2dTex.outUV, newFile.uv)
+    pmc.connectAttr(new2dTex.outUvFilterSize, newFile.uvFilterSize)
+    newFile.fileTextureName.set(tex_filepath)
+
+    return newFile, new2dTex
+
+
+def create_Shader(shader_name, PDX_material, texture_dir):
+    new_shader = pmc.shadingNode('phong', asShader=True, name=shader_name)
+    new_shadinggroup= pmc.sets(renderable=True, noSurfaceShader=True, empty=True, name='{}_SG'.format(shader_name))
+    pmc.connectAttr(new_shader.outColor, new_shadinggroup.surfaceShader)
+
+    # TODO: should be an enum datatype, need to parse the possible engine/material combinations from clausewitz.txt
+    pmc.addAttr(longName='shader', dataType='string')
+    new_shader.shader.set(PDX_material.shader)
+
+    texture_dict = PDX_material.get_textures()
+
+    if texture_dict.get('diff'):
+        texture_path = os.path.join(texture_dir, PDX_material.diff[0])
+        new_file, _ = create_FileTexture(texture_path)
+        pmc.connectAttr(new_file.outColor, new_shader.color)
+
+    if texture_dict.get('n'):
+        texture_path = os.path.join(texture_dir, PDX_material.n[0])
+        new_file, _ = create_FileTexture(texture_path)
+        bump2d = pmc.shadingNode('bump2d', asUtility=True)
+        bump2d.bumpDepth.set(0.1)
+        new_file.alphaIsLuminance.set(True)
+        pmc.connectAttr(new_file.outAlpha, bump2d.bumpValue)
+        pmc.connectAttr(bump2d.outNormal, new_shader.normalCamera)
+
+    if texture_dict.get('spec'):
+        texture_path = os.path.join(texture_dir, PDX_material.spec[0])
+        new_file, _ = create_FileTexture(texture_path)
+        pmc.connectAttr(new_file.outColor, new_shader.specularColor)
+
+    return new_shader, new_shadinggroup
+
+
+def create_Locator(PDX_locator):
+    # create locator
+    new_loc = pmc.spaceLocator()
+    pmc.select(new_loc)
+    pmc.rename(new_loc, PDX_locator.name)
+
+    # set attributes
+    obj = OpenMaya.MObject()
+    selList = OpenMaya.MSelectionList()
+    selList.add(new_loc.name())
+    selList.getDependNode(0, obj)
+
+    m_FnXform = OpenMaya.MFnTransform(obj)
+    # translation
+    vector = OpenMaya.MVector(PDX_locator.p[0], PDX_locator.p[1], PDX_locator.p[2])
+    space = OpenMaya.MSpace.kTransform
+    m_FnXform.setTranslation(vector, space)
+    # rotation
+    m_FnXform.setRotationQuaternion(PDX_locator.q[0], PDX_locator.q[1], PDX_locator.q[2], PDX_locator.q[3])
+
+
+def create_Mesh(PDX_mesh):
+    # name and namespace
+    mesh_name = PDX_mesh.name.split(':')[-1]        # TODO: check for identical mesh names, this just means material will be different
+    namespaces = PDX_mesh.name.split(':')[:-1]      # TODO: setup namespaces properly
+
+    # vertices
+    verts = PDX_mesh.p      # flat list of co-ordinates, verts[:2] = vtx[0]
+
+    # normals
+    norms = None
+    if hasattr(PDX_mesh, 'n'):
+        norms = PDX_mesh.n      # flat list of co-ordinates, norms[:2] = nrm[0]
+
+    # triangles
+    tris = PDX_mesh.tri     # flat list of connections, tris[:3] = face[0]
+
+    # UVs
+    uv_0 = None
+    uv_1 = None
+    if hasattr(PDX_mesh, 'u0'):
+        uv_0 = PDX_mesh.u0
+    if hasattr(PDX_mesh, 'u1'):
+        uv_1 = PDX_mesh.u1
+
+    # material
+    mat = PDX_mesh.material
+
+
+    # create the data structures for mesh and transform
+    m_FnMesh = OpenMaya.MFnMesh()
+    m_DagMod = OpenMaya.MDagModifier()
+    new_object = m_DagMod.createNode('transform')
+
+    # build the following arguments for the MFnMesh.create() function
+    # numVertices, numPolygons, vertexArray, polygonCounts, polygonConnects, uArray, vArray, new_object
+
+    # vertices
+    numVertices = 0
+    vertexArray = OpenMaya.MFloatPointArray()   # array of points
+    for i in xrange(0, len(verts), 3):
+        v = OpenMaya.MFloatPoint(verts[i], verts[i+1], verts[i+2])
+        vertexArray.append(v)
+        numVertices += 1
+
+    # faces
+    numPolygons = len(tris) / 3
+    polygonCounts = OpenMaya.MIntArray()    # count of vertices per poly
+    for i in range(0, numPolygons):
+        polygonCounts.append(3)
+    # OpenMaya.MScriptUtil.createIntArrayFromList([3]*numPolygons, polygonCounts)
+
+    # vert connections
+    polygonConnects = OpenMaya.MIntArray()
+    for item in tris:
+        polygonConnects.append(item)
+    # OpenMaya.MScriptUtil.createIntArrayFromList(tris, polygonConnects)
+
+    # UVs
+    uArray = OpenMaya.MFloatArray()
+    vArray = OpenMaya.MFloatArray()
+    if uv_0:
+        for i in xrange(0, len(uv_0), 2):
+            uArray.append(uv_0[i])
+            vArray.append(1 - uv_0[i+1])        # flip the UV coords in V!
+
+
+    # create the new mesh
+    m_FnMesh.create(numVertices, numPolygons, vertexArray, polygonCounts, polygonConnects, uArray, vArray, new_object)
+    m_FnMesh.setName(mesh_name)
+    m_DagMod.doIt()     # sets up the transform parent to the mesh shape
+
+    # apply the vertex normal data
+    if norms:
+        normalsIn = OpenMaya.MVectorArray()     # array of vectors
+        for i in xrange(0, len(norms), 3):
+            n = OpenMaya.MVector(norms[i], norms[i+1], norms[i+2])
+            normalsIn.append(n)
+        vertexList = OpenMaya.MIntArray()       # matches normal to vert by index
+        for i in range(0, numVertices):
+            vertexList.append(i)
+        m_FnMesh.setVertexNormals(normalsIn, vertexList)
+
+    # apply the UV data
+    if uv_0:
+        uvCounts = OpenMaya.MIntArray()
+        for i in range(0, numPolygons):
+            uvCounts.append(3)
+        # OpenMaya.MScriptUtil.createIntArrayFromList(verts_per_poly, uvCounts)
+        uvIds = OpenMaya.MIntArray()
+        for item in tris:
+            uvIds.append(item)
+        # OpenMaya.MScriptUtil.createIntArrayFromList(raw_tris, uvIds)
+        m_FnMesh.assignUVs(uvCounts, uvIds, 'map1')     # note bulk assignment via .assignUVs only works to the default UV set!
+
+    # setup the material
+    shader_name = 'Phong_'+mesh_name
+    texture_dir = os.path.split(filepath)[0]
+    shader, s_group = create_Shader(shader_name, mat, texture_dir)
+
+    pmc.select(mesh_name)
+    pmc.hyperShade(assign=s_group)
+
+
+def create_Skeleton(bone_data):
+    pmc.select(clear=True)
+    # keep track of bones as we create them
+    bone_list = [None for _ in range(0, len(bone_data))]
+
+    for bone in bone_data:
+        index = bone.ix[0]
+        transform = bone.tx
+        parent = getattr(bone, 'pa', None)
+            
+        # create joint
+        new_bone = pmc.joint()
+        pmc.select(new_bone)
+        pmc.rename(new_bone, bone.name)     # TODO: setup namespaces properly
+        bone_list[index] = new_bone
+        new_bone.radius.set(0.5)
+        
+        # set transform
+        mat = pmdt.Matrix(
+            transform[0], transform[1], transform[2], 0.0,
+            transform[3], transform[4], transform[5], 0.0,
+            transform[6], transform[7], transform[8], 0.0,
+            transform[9], transform[10], transform[11], 1.0
+        )
+        pmc.xform(matrix = mat.inverse())   # set transform to inverse of matrix in world-space
+        pmc.select(clear=True)
+        
+        # connect to parent
+        if parent is not None:
+            parent_bone = bone_list[parent[0]]
+            pmc.connectJoint(new_bone, parent_bone, parentMode=True)
+
+
+""" ================================================================================================
+    Main.
+====================================================================================================
+"""
+
+# read the data
+filepath = r"C:\Users\Ross\Documents\GitHub\io_pdx_mesh\test files\fallen_empire_large_warship.mesh"
+filepath = r"C:\Users\Ross\Documents\GitHub\io_pdx_mesh\test files\JAP_01.mesh"
+
+asset = pdx_data.read_meshfile(filepath, to_stdout=True)
+
+for mesh in asset.meshes:
+    create_Mesh(mesh)
+
+for mesh in asset.meshes:
+    create_Skeleton(mesh.skeleton)
+    
+for loc in asset.locators:
+    create_Locator(loc)
---
+++ pdx_data.py
@@ -0,0 +1,471 @@
+"""
+    Paradox asset files, read/write binary data.
+    
+    author : ross-g
+"""
+
+import os, sys
+import struct
+import json
+
+clear = lambda: os.system('cls')
+
+
+""" ================================================================================================
+    Variables.
+====================================================================================================
+"""
+
+SETTINGS_FILE = os.path.join(os.environ['HOME'], 'Documents', 'Paradox Interactive', 'PdxExporter', 'settings', 'clausewitz.txt')
+
+
+""" ================================================================================================
+    Functions for reading binary data files.
+====================================================================================================
+"""
+
+def readBinaryFile(filepath):
+    with open(filepath, 'rb') as file:
+        data = file.read()
+
+    return data
+
+def parseBinary(bdata):
+    # determine the file length
+    eof = len(bdata)
+
+    # set inital position in file to skip '@@b@'
+    pos = 4
+    objdepth = 0
+
+    # scan through until EOF
+    obj_list = [['file']]
+    while pos < eof:
+        if struct.unpack('c', bdata[pos])[0] == '!':
+            # we have a property
+            if objdepth == None: objdepth = 0
+            prop_name, prop_values, pos = parseProperty(bdata, pos)
+            parent = obj_list[objdepth][-1]
+            print "  "*objdepth+"  property:", prop_name, "("+parent+")", "\n", prop_values
+
+        elif struct.unpack('c', bdata[pos])[0] == '[':
+            # we have an object
+            obj_name, objdepth, pos = parseObject(bdata, pos)
+            if len(obj_list) == objdepth + 1:
+                obj_list[objdepth].append(obj_name)
+            else:
+                obj_list.append([obj_name])
+            parent = obj_list[objdepth-1][-1]
+            print "  "*objdepth+"object:", obj_name, "("+parent+")"
+
+        else:
+            raise NotImplementedError("Unknown object encountered.")
+
+def parseProperty(bdata, pos):
+    # starting at '!'
+    pos += 1
+
+    # get length of property name
+    prop_name_length = struct.unpack('b', bdata[pos])[0]
+    pos += 1
+
+    # get property name as string
+    prop_name = parseString(bdata, pos, prop_name_length)
+    pos += prop_name_length
+
+    # get property data
+    prop_values, pos = parseData(bdata, pos)
+
+    return (prop_name, prop_values, pos)
+
+def parseObject(bdata, pos):
+    # skip and record any repeated '[' characters
+    objdepth = 0
+    while struct.unpack('c', bdata[pos])[0] == '[':
+        # TODO: we are assuming the object hierarchy is predefined, unknown hierarchy might parse incorrectly
+        objdepth += 1
+        pos += 1
+
+    # get object name as string
+    obj_name = ''
+    # we don't know the string length, so look for an ending byte of zero
+    while struct.unpack('b', bdata[pos])[0] != 0:
+        obj_name += struct.unpack('c', bdata[pos])[0]
+        pos += 1
+    
+    # skip the ending zero byte
+    pos += 1
+
+    return (obj_name, objdepth, pos)
+
+def parseString(bdata, pos, length):
+    string = struct.unpack('c'*length, bdata[pos:pos+length])
+
+    # check if the ending byte is zero and remove if so
+    if string[-1] == chr(0):
+        string = string[:-1]
+
+    return ''.join(string)
+
+def parseData(bdata, pos):
+    datavalues = []
+    # determine the  data type
+    datatype = struct.unpack('c', bdata[pos])[0]
+
+    if datatype == 'i':
+        # handle integer data
+        pos += 1
+
+        # count
+        size = struct.unpack('i', bdata[pos:pos+4])[0]
+        pos += 4
+
+        # values
+        for i in range(0, size):
+            val = struct.unpack('i', bdata[pos:pos+4])[0]
+            datavalues.append(val)
+            pos += 4
+
+    elif datatype == 'f':
+        # handle float data
+        pos += 1
+
+        # count
+        size = struct.unpack('i', bdata[pos:pos+4])[0]
+        pos += 4
+        
+        # values
+        for i in range(0, size):
+            val = struct.unpack('f', bdata[pos:pos+4])[0]
+            datavalues.append(val)
+            pos += 4
+
+    elif datatype == 's':
+        # handle string data
+        pos += 1
+
+        # count
+        size = struct.unpack('i', bdata[pos:pos+4])[0]
+        # TODO: we are assuming that we always have a count of 1 string, not an array of multiple strings
+        pos += 4
+
+        # string length
+        str_data_length = struct.unpack('i', bdata[pos:pos+4])[0]
+        pos += 4
+
+        # value
+        val = parseString(bdata, pos, str_data_length)
+        datavalues.append(val)
+        pos += str_data_length
+
+    else:
+        raise NotImplementedError("Unknown data type encountered.")
+
+    return (datavalues, pos)
+
+def read_meshfile(filepath, to_stdout=False, full=False):
+    """
+        Reads through a .mesh file and instantiates PDX... classes based on the file hierarchy
+
+        This might need re-architecting, using nested lists to create the hierarchy leads to lots of arbitrary traversing the list back up the hierarchy
+        where we need to reference anything other than the immediate parent, this could also be problematic where we have multiples of the same object
+        type in a file.
+        Possibly use tree structure?  https://gist.github.com/hrldcpr/2012250
+        Possibly use named tuples? ordered dict?  https://docs.python.org/2.7/library/collections.html
+    """
+    # read the data
+    fdata = readBinaryFile(filepath)
+
+    # create the asset class, this represents the whole file
+    pdxasset = PDXmodel(os.path.split(filepath)[1])
+    # create a nested list to store the object hierarchy, this get populated by strings or classes 
+    # depending on how we're handing the specific object
+    obj_list = [['file']]
+    obj_depth = 0
+    
+    
+    # determine the file length
+    eof = len(fdata)
+    # set position in file to skip '@@b@'
+    if fdata[:4] == '@@b@':
+        pos = 4
+    else:
+        raise StandardError("Unknown file header")
+
+    # parse through until EOF
+    current_object = None
+    while pos < eof:
+        # we have a property
+        if struct.unpack('c', fdata[pos])[0] == '!':
+            # check the property type and values
+            prop_name, prop_values, pos = parseProperty(fdata, pos)
+            # check which object has this property
+            parent_object = obj_list[obj_depth][-1]
+
+            # assign property values to the parent object
+            try:
+                setattr(parent_object, prop_name, prop_values)
+            except:
+                # special case some properties, assign values to the grandparent
+                if parent_object == 'aabb':
+                    parent_mesh = obj_list[obj_depth-1][-1]
+                    setattr(parent_mesh, parent_object+prop_name, prop_values)
+
+            if to_stdout:
+                if full:
+                    print "  "*obj_depth+"  prop:", prop_name, "("+str(parent_object)+")", "\n", prop_values
+                else:
+                    print "  "*obj_depth+"  prop:", prop_name, "("+str(parent_object)+")"
+
+        # we have an object
+        elif struct.unpack('c', fdata[pos])[0] == '[':
+            # check the object type and hierarchy depth
+            obj_name, obj_depth, pos = parseObject(fdata, pos)
+            # check which object contains this object in the hierarcy
+            parent_object = obj_list[obj_depth-1][-1]
+
+            # determine if this is a known object type, then instantiate it
+            if obj_name == 'mesh':
+                current_object = PDXmesh(parent_object)
+                pdxasset.meshes.append(current_object)
+            elif obj_name == 'material':
+                current_object = PDXmaterial()
+                parent_object.material = current_object
+            elif obj_name == 'skin':
+                current_object = PDXskin()
+                parent_object.skin = current_object
+            else:
+                # some obects have type defined by their parent, instantiate them
+                if parent_object == 'locator':
+                    current_object = PDXlocator(obj_name)
+                    pdxasset.locators.append(current_object)
+                elif parent_object == 'skeleton':
+                    # if not hasattr(parent_object, 'skeleton'):
+                    #     parent_object.skeleton = []
+                    current_object = PDXbone(obj_name)
+                    parent_object = obj_list[obj_depth-1][-2]       # TODO this feels sloppy, we arbitrarily traverese the list back one more item at the right depth
+                    parent_object.skeleton.append(current_object)
+                # otherwise just store the name string instead
+                else:
+                    current_object = obj_name
+
+            try:
+                obj_list[obj_depth].append(current_object)
+            except:
+                obj_list.append([current_object])
+
+            if to_stdout: print "  "*obj_depth+"obj:", current_object, "("+str(parent_object)+")", obj_depth
+
+        else:
+            raise NotImplementedError("Unknown object encountered.")
+
+    return pdxasset
+
+
+""" ================================================================================================
+    Classes describing PDX objects.
+====================================================================================================
+"""
+
+class PDXmodel(object):
+    """
+        mesh    (object)
+        locator    (object)
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        self.meshes = []
+        self.locators = []
+
+    def __str__(self):
+        string = '{}'.format(self.filename)
+        string += '\n\tmeshes:'
+        for m in self.meshes:
+            string += '\n\t\t{}'.format(m)
+        string += '\n\tlocators:'
+        for l in self.locators:
+            string += '\n\t\t{}'.format(l)
+
+        return string
+
+class PDXmesh(object):
+    """
+        object    (object)
+            shape    (object)
+                mesh    (object)
+                    p    (float)  verts
+                    n    (float)  normals
+                    ta    (float)  tangents
+                    u    (float)  UVs
+                    tri    (int)  triangles
+                    aabb    (object)
+                        min    (float)  min bounding box
+                        max    (float)  max bounding box
+                    material    (object)
+                    skin    (object)
+                        bones    (int)  used bones
+                        ix    (int)  skin ids
+                        w    (float)  skin weights
+                skeleton    (object)
+    """
+    def __init__(self, name):
+        self.name = name    # shape node name
+        self.p = None
+        self.n = None
+        self.ta = None
+        self.u = None
+        self.tri = None
+        self.aabbmin = None
+        self.aabbmax = None
+        self.material = None    # a mesh only has one material
+        self.skin = None        # a mesh only has one skin
+        self.skeleton = []    # a list of bone objects
+
+    def __str__(self):
+        # return json.dumps(self.__dict__, skipkeys=True, indent=4, sort_keys=True)
+        return 'PDXmesh-{}'.format(self.name)
+
+class PDXmaterial(object):
+    """
+        material    (object)
+            shader    (string)  shader name
+            diff    (string)  texture_diffuse
+            n    (string)  texture_normal
+            spec    (string)  texture_spec
+    """
+    def __init__(self):
+        self.shader = None
+        self.diff = None
+        self.n = None
+        self.spec = None
+
+    def get_textures(self):
+        texture_dict = {key:val for (key,val) in (self.__dict__).items() if key != 'shader'}
+        return texture_dict
+        
+    def __str__(self):
+        return 'PDXmaterial'
+
+class PDXskin(object):
+    """
+        skin    (object)
+            bones    (int)  used bones?
+            ix    (int)  skin ids
+            w    (float)  skin weights
+    """
+    def __init__(self):
+        pass
+        self.bones = None
+        self.ix = None
+        self.w = None
+        
+    def __str__(self):
+        return 'PDXskin'
+
+class PDXbone(object):
+    """
+        bone    (object)
+            ix    (int)  index
+            pa    (int)  parent index, omitted for root
+            tx    (float)  transform, 3*4 matrix
+    """
+    def __init__(self, name):
+        self.name = name
+        self.ix = None
+        self.pa = None
+        self.tx = None
+        
+    def __str__(self):
+        return 'PDXbone-{}'.format(self.name)
+
+class PDXlocator(object):
+    """
+        locator    (object)
+            node    (object)
+                p    (float)  position?
+                q    (float)  quarternion?
+                pa    (string)  parent bone?
+    """
+    def __init__(self, name):
+        self.name = name
+        self.p = None
+        self.q = None
+        self.pa = None
+        
+    def __str__(self):
+        return 'PDXlocator-{}'.format(self.name)
+
+
+
+""" ================================================================================================
+    Main.
+====================================================================================================
+"""
+
+if __name__ == '__main__':
+    """
+        When run like this we just print the contents of the mesh file to stdout
+    """
+    clear()
+    a_file = sys.argv[1]
+    if len(sys.argv) == 3:
+        full = sys.argv[2]
+    else:
+        full = False
+    # a_file = r"C:\Users\Ross\Documents\GitHub\io_pdx_mesh\test files\test_object.mesh"
+    # a_file = r"C:\Users\Ross\Documents\GitHub\io_pdx_mesh\test files\scan_detail.mesh" # SIMPLE locator, skeleton, skin
+    # a_file = r"C:\Users\Ross\Documents\GitHub\io_pdx_mesh\test files\robot_01_portrait.mesh" # COMPLEX locators, skeletons, skins
+    # a_file = r"C:\Users\Ross\Documents\GitHub\io_pdx_mesh\test files\combat_items\torpedo.mesh" # multiple meshes, collision shader
+
+    print read_meshfile(a_file, to_stdout=True, full=full)
+
+
+
+"""
+.mesh file format
+
+General binary format is:
+    data description
+    data type
+    depth of data
+    data content
+
+=======================================================================================================================
+    header    (@@b@ for binary, @@t@ for text)?
+    pdxasset    (int)  number of assets?
+        object    (object)  parent item for all 3D objects
+            shape    (object)
+                mesh    (object)
+                    ...  multiple meshes per shape, used for different materials
+                mesh    (object)
+                    ...
+                mesh    (object)
+                    p    (float)  verts
+                    n    (float)  normals
+                    ta    (float)  tangents
+                    u    (float)  UVs
+                    tri    (int)  triangles
+                    aabb    (object)
+                        min    (float)  min bounding box
+                        max    (float)  max bounding box
+                    material    (object)
+                        shader    (string)  shader name
+                        diff    (string)  texture_diffuse
+                        n    (string)  texture_normal
+                        spec    (string)  texture_spec
+                    skin    (object)
+                        bones    (int)  used bones?
+                        ix    (int)  skin ids
+                        w    (float)  skin weights
+                skeleton    (object)
+                    bone    (object)
+                        ix    (int)  index
+                        pa    (int)  parent index, omitted for root
+                        tx    (float)  transform, 3*4 matrix
+        locator    (object)  parent item for all locators
+            node    (object)
+                p    (float)  position?
+                q    (float)  quarternion?
+                pa    (string)  parent bone?
+=======================================================================================================================
+"""
