b2c9239acbb997508a90aef29c413e0963b8d064
Ross <ross.garfoot@gmail.com>
2022-10-24 15:57:24 +0100

    Fixed first time np import fail after a new install

--- external/__init__.py
+++ external/__init__.py
@@ -33,6 +33,7 @@ try:
 except ImportError:
     # check if numpy has been installed to external/numpy_maya
     numpy_maya = path.join(external_path, "numpy_maya")
+    pathlib.Path(numpy_maya).mkdir(exist_ok=True)
     site.addsitedir(numpy_maya)
     try:
         import numpy  # noqa
@@ -42,12 +43,38 @@ except ImportError:
         EXTERNAL_LOG.info("Installing numpy package")
         mayapy = path.join(path.dirname(sys.executable), "mayapy.exe")
 
-        # since Maya 2022 (Py3) numpy can just be installed via pip, and pip is included
+        # pip is included with Maya 2022+, but needs to be installed otherwise
+        try:
+            import pip
+
+            EXTERNAL_LOG.info("Found pip at: {}".format(path.dirname(pip.__file__)))
+
+        except ImportError:
+
+            def download_get_pip():  # Py2 only
+                file = urllib.URLopener()
+                pip_url = "https://bootstrap.pypa.io/pip/{}.{}/get-pip.py".format(*sys.version_info[:2])
+                fpath, _ = file.retrieve(pip_url, path.join(os.environ["LOCALAPPDATA"], "get-pip.py"))
+                return fpath
+
+            # try installing pip (after downloading get-pip.py)
+            try:
+                # first get pip working
+                get_pip = download_get_pip()
+                subprocess.check_call([mayapy, get_pip], creationflags=subprocess.CREATE_NEW_CONSOLE)
+                import pip
+
+                EXTERNAL_LOG.info("Installed pip at: {}".format(path.dirname(pip.__file__)))
+
+            except (subprocess.CalledProcessError, ImportError):
+                numpy = None
+                EXTERNAL_LOG.warning("Failed to install pip (from get-pip.py)")
+
+        # since Maya 2022 (Py3) numpy can just be installed via pip
         if six.PY3 and path.isfile(mayapy):
             try:
                 subprocess.check_call(
-                    [mayapy, "-m", "pip", "install", "numpy"],
-                    creationflags=subprocess.CREATE_NEW_CONSOLE
+                    [mayapy, "-m", "pip", "install", "numpy"], creationflags=subprocess.CREATE_NEW_CONSOLE
                 )
                 import numpy  # noqa
 
@@ -57,29 +84,8 @@ except ImportError:
                 numpy = None
                 EXTERNAL_LOG.warning("Failed to install Numpy (from pip)")
 
-        # otherwise try installing pip (after downloading get-pip.py) and loading numpy from a bespoke wheel
+        # otherwise try installing numpy from a bespoke wheel
         elif path.isfile(mayapy):
-            def download_get_pip():  # Py2 only
-                file = urllib.URLopener()
-                pip_url = "https://bootstrap.pypa.io/pip/{}.{}/get-pip.py".format(*sys.version_info[:2])
-                fpath, _ = file.retrieve(pip_url, path.join(os.environ["LOCALAPPDATA"], "get-pip.py"))
-                return fpath
-
-            try:
-                # first get pip working
-                get_pip = download_get_pip()
-                subprocess.check_call(
-                    [mayapy, get_pip],
-                    creationflags=subprocess.CREATE_NEW_CONSOLE
-                )
-                import pip
-
-                EXTERNAL_LOG.info("Installed pip at: {}".format(path.dirname(pip.__file__)))
-
-            except (subprocess.CalledProcessError, ImportError):
-                numpy = None
-                EXTERNAL_LOG.warning("Failed to install pip (from get-pip.py)")
-
             try:
                 # # then install the bundled numpy wheel
                 # numpy_wheel = path.join(numpy_maya, "numpy-1.9.2-cp27-none-win_amd64.whl")
@@ -89,8 +95,19 @@ except ImportError:
                 # )
                 # then install the numpy wheel from 3rd party package index
                 subprocess.check_call(
-                    [mayapy, "-m", "pip", "install", "--upgrade",
-                    "--index-url", "https://pypi.anaconda.org/carlkl/simple", "numpy", "--target", numpy_maya],
+                    [
+                        mayapy,
+                        "-m",
+                        "pip",
+                        "install",
+                        "--upgrade",
+                        "--index-url",
+                        "https://pypi.anaconda.org/carlkl/simple",
+                        "numpy",
+                        "--target",
+                        numpy_maya,
+                    ],
+                    creationflags=subprocess.CREATE_NEW_CONSOLE,
                 )
                 import numpy  # noqa
 
