56b7712df67b2d704a290c2ab8f8f77435a042d9
Ross <ross.garfoot@gmail.com>
2022-12-04 18:50:57 +0000

    Bump version for release

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ bl_info = {
     "support": "COMMUNITY",
     "blender": (2, 83, 0),
     "maya": (2018),
-    "version": (0, 8),
+    "version": (0, 9),
     "warning": "this add-on is beta",
     "project_name": "io_pdx_mesh",
     "project_url": "https://github.com/ross-g/io_pdx_mesh",
